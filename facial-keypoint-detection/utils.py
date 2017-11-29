243402a80f946ebe7f53638cda68f9293775769c
Max Lee <max@vcnc.co.kr>
2017-11-29 21:12:49 +0900

    각종 함수들은 .py 로 빼고 notebook 에서는 실행만 시키는 쪽으로 수정

---
+++ facial-keypoint-detection/utils.py
@@ -0,0 +1,95 @@
+import math
+import os
+import time
+
+import numpy as np
+from pandas.io.parsers import read_csv
+from sklearn.utils import shuffle
+import tensorflow as tf
+
+
+def load(test=False, cols=None, verbose=False):
+    FTRAIN = os.environ["FTRAIN"]
+    FTEST = os.environ["FTEST"]
+
+    fname = FTEST if test else FTRAIN
+    df = read_csv(fname)
+
+    df["Image"] = df["Image"].apply(lambda im: np.fromstring(im, sep=' '))
+
+    if cols:
+        df = df[list(cols) + ["Image"]]
+    if verbose:
+        print(df.count())
+
+    df = df.dropna()
+
+    X = np.vstack(df["Image"].values) / 255.
+    X = X.astype(np.float32)
+
+    if not test:
+        y = df[df.columns[:-1]].values
+        y = (y - 48) / 48
+        X, y = shuffle(X, y, random_state=42)
+        y = y.astype(np.float32)
+    else:
+        y = None
+
+    return X, y
+
+
+def plot_sample(x, y, axis):
+    # 테스트 이미지에 keypoint 를 그려보는것도 블로그 포스트의 코드를 그대로 사용했다.
+    #  크기를 키우고 (figsize), marker 크기와(s), 색깔에(c) 변화를 줬다.
+    img = x.reshape(96, 96)
+    axis.imshow(img, cmap="gray")
+    axis.scatter(y[0::2] * 48 + 48, y[1::2] * 48 + 48, marker='x', s=50, c='r')
+
+
+def train(num_epoch, X, y, X_test):
+    """
+    https://www.tensorflow.org/get_started/mnist/mechanics
+    tensorflow feed forward example 로 검색해서 나온 [코드를](https://gist.github.com/vinhkhuc/e53a70f9e5c3f55852b0) 참고해봤다.
+    mini-batch training 에 필요한 hyper-parameter 들은 [Kaggle 이 소개한 블로그 포스트를](http://danielnouri.org/notes/2014/12/17/using-convolutional-neural-nets-to-detect-facial-keypoints-tutorial/) 따라해보자.
+    위의 블로그 포스트에서도 첫번째 model 에서는 full-batch 로 학습시키는것 같으므로, 우선은 mini-batch 는 구현하지 않고 진행해보자.
+    """
+
+    images = tf.placeholder(tf.float32, shape=(None, 9216))
+    truth = tf.placeholder(tf.float32, shape=(None, 30))
+
+    with tf.name_scope("hidden"):
+        weights = tf.Variable(tf.truncated_normal([9216, 100], stddev=1.0 / math.sqrt(float(9216))), name="weights")
+        biases = tf.Variable(tf.zeros([100]), name="biases")
+        hidden = tf.nn.relu(tf.matmul(images, weights) + biases)
+    with tf.name_scope("mse_linear"):
+        weights = tf.Variable(tf.truncated_normal([100, 30], stddev=1.0 / math.sqrt(float(100))), name="weights")
+        biases = tf.Variable(tf.zeros([30]), name="biases")
+        predict = tf.matmul(hidden, weights) + biases
+
+    loss = tf.losses.mean_squared_error(truth, predict)
+    # MNIST 쪽 예제에서는 global_step 을 만들어서 추가로 넣어줬었다.
+    # global_step = tf.Variable(0, name="global_step", trainable=False)
+    # train_op = optimizer.minimize(loss, global_step=global_step)
+    optimizer = tf.train.MomentumOptimizer(learning_rate=0.01, momentum=0.9, use_nesterov=True).minimize(loss)
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        # tf.summary.scalar("loss", loss)
+        # merged = tf.summary.merge_all()
+        # train_writer = tf.summary.FileWriter(summary_file_path(postfix), sess.graph)
+
+        feed_dict = {images: X, truth: y}
+
+        for e in range(num_epoch):
+            start_ms = int(time.time() * 1000)
+            sess.run(optimizer, feed_dict=feed_dict)
+            # summary, training_loss = sess.run([merged, loss], feed_dict=feed_dict)
+            training_loss = sess.run(loss, feed_dict=feed_dict)
+            # train_writer.add_summary(summary, e)
+            elapsed_ms = int(time.time() * 1000) - start_ms
+            print("Epoch: {:4d}\tTraining Loss: {:.7f}\tElapsed Time(ms): {:6d}".format(e, training_loss, elapsed_ms))
+
+        # train_writer.close()
+        y_pred = sess.run(predict, feed_dict={images: X_test})
+
+    return y_pred
