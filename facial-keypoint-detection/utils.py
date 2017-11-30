f1e9e03f31a3ebc65904944b933e9fa08141eb24
Max Lee <max@vcnc.co.kr>
2017-11-30 20:38:48 +0900

    training 할때 validation loss 를 구하려는 시도
    - TensorBoard 에서 training/validation loss 를 동시에 볼 수 없나...

--- facial-keypoint-detection/utils.py
+++ facial-keypoint-detection/utils.py
@@ -46,14 +46,23 @@ def plot_sample(x, y, axis):
     axis.scatter(y[0::2] * 48 + 48, y[1::2] * 48 + 48, marker='x', s=50, c='r')
 
 
-def train(num_epoch, X, y, X_test, name=""):
+def train(num_epoch, X, y, X_test, name="", ratio_val=0.2):
     """
     https://www.tensorflow.org/get_started/mnist/mechanics
     tensorflow feed forward example 로 검색해서 나온 [코드를](https://gist.github.com/vinhkhuc/e53a70f9e5c3f55852b0) 참고해봤다.
     mini-batch training 에 필요한 hyper-parameter 들은 [Kaggle 이 소개한 블로그 포스트를](http://danielnouri.org/notes/2014/12/17/using-convolutional-neural-nets-to-detect-facial-keypoints-tutorial/) 따라해보자.
     위의 블로그 포스트에서도 첫번째 model 에서는 full-batch 로 학습시키는것 같으므로, 우선은 mini-batch 는 구현하지 않고 진행해보자.
     """
-    tf.reset_default_graph()  # notebook 에서 train 함수를 호출할때 InvalidArgumentError 가 발생하는걸 방지하기 위함
+    # notebook 에서 train 함수를 호출할때 InvalidArgumentError 가 발생하는걸 방지하기 위함
+    tf.reset_default_graph()
+
+    # X 가 매번 shuffle 된다고 가정하고 연속된 2 덩어리로 나눕니다.
+    num_total, _ = X.shape
+    num_val = int(num_total * ratio_val)
+    X_train = X[:num_val, :]
+    y_train = y[:num_val, :]
+    X_val = X[num_val:, :]
+    y_val = y[num_val:, :]
 
     images = tf.placeholder(tf.float32, shape=(None, 9216))
     truth = tf.placeholder(tf.float32, shape=(None, 30))
@@ -68,6 +77,7 @@ def train(num_epoch, X, y, X_test, name=""):
         predict = tf.matmul(hidden, w2) + b2
 
     loss = tf.losses.mean_squared_error(truth, predict)
+    loss_val = tf.losses.mean_squared_error(truth, predict)
     # MNIST 쪽 예제에서는 global_step 을 만들어서 추가로 넣어줬었다.
     # global_step = tf.Variable(0, name="global_step", trainable=False)
     # train_op = optimizer.minimize(loss, global_step=global_step)
@@ -76,20 +86,23 @@ def train(num_epoch, X, y, X_test, name=""):
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
 
-        tf.summary.scalar("loss", loss)
-        merged = tf.summary.merge_all()
+        training_summary = tf.summary.scalar("loss", loss)
+        validation_summary = tf.summary.scalar("loss_val", loss_val)
         file_path = "{}_{}".format(datetime.now().strftime("%Y-%m-%d_%H:%M:%S"), name)
         train_writer = tf.summary.FileWriter(os.path.join(os.getcwd(), "summaries", file_path))
 
-        feed_dict = {images: X, truth: y}
+        feed_dict_train = {images: X_train, truth: y_train}
+        feed_dict_val = {images: X_val, truth: y_val}
 
         for e in range(num_epoch):
             start_ms = int(time.time() * 1000)
-            sess.run(optimizer, feed_dict=feed_dict)
-            summary, training_loss = sess.run([merged, loss], feed_dict=feed_dict)
+            sess.run(optimizer, feed_dict=feed_dict_train)
+            training_summ, training_loss = sess.run([training_summary, loss], feed_dict=feed_dict_train)
+            validation_summ, _ = sess.run([validation_summary, loss_val], feed_dict=feed_dict_val)
             elapsed_ms = int(time.time() * 1000) - start_ms
             print("Epoch: {:4d}\tTraining Loss: {:.7f}\tElapsed Time(ms): {:6d}".format(e, training_loss, elapsed_ms))
-            train_writer.add_summary(summary, e)
+            train_writer.add_summary(training_summ, e)
+            train_writer.add_summary(validation_summ, e)
             train_writer.flush()
 
         y_pred = sess.run(predict, feed_dict={images: X_test})
